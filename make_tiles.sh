220a98ead457977dd94f9e97067defb7b5016770
George Angelopoulos <george@usermod.net>
2014-09-22 13:52:23 +0300

    more verbosity

--- make_tiles.sh
+++ make_tiles.sh
@@ -9,9 +9,12 @@ WEBDIR="/var/www/maps.ayntest.net/tiles/"
 
 TILE_SIZE=344
 
+BACKUPFILE="map-$(date +%s).png"
+
 
 # backup current map
-cp "$FULL_MAP" "map-$(date +%s).png"
+echo "creating $BACKUPFILE backup"
+cp "$FULL_MAP" "$BACKUPFILE"
 
 echo 'setting up temporary directory'
 [ -d $TMPDIR ] && rm -r $TMPDIR
@@ -20,15 +23,18 @@ mkdir $TMPDIR/24
 mkdir $TMPDIR/23
 mkdir $TMPDIR/22
 
+echo 'starting conversions'
 convert -verbose -crop "${TILE_SIZE}x$TILE_SIZE" +repage "$FULL_MAP"  $TMPDIR/24/tiles_%d.png
 convert -verbose -scale 50% "$FULL_MAP" $TMPDIR/map23.png
 convert -verbose -crop "${TILE_SIZE}x$TILE_SIZE" +repage $TMPDIR/map23.png  $TMPDIR/23/tiles_%d.png
 convert -verbose -scale 25% "$FULL_MAP" $TMPDIR/map22.png
 convert -verbose -crop "${TILE_SIZE}x$TILE_SIZE" +repage $TMPDIR/map22.png  $TMPDIR/22/tiles_%d.png
 
+echo 'renaming tiles'
 $RENAME_SCRIPT 8256 $TILE_SIZE $TMPDIR/24
 $RENAME_SCRIPT 4128 $TILE_SIZE $TMPDIR/23
 $RENAME_SCRIPT 2064 $TILE_SIZE $TMPDIR/22
 
+echo 'copying to webdir'
 rsync -avh --progress $TMPDIR/ $WEBDIR
 rm -r $TMPDIR
